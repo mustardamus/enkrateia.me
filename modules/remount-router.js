34ced597aa584e132ca03c3b15cdda4bffc8042c
Sebastian Senf <contact@mustardamus.com>
2017-08-28 13:25:41 +0200

    get rid of cross-env, set isDev option in remount router

--- modules/remount-router.js
+++ modules/remount-router.js
@@ -3,7 +3,10 @@ const remountRouter = require('remount-router')
 
 module.exports = function (moduleOptions) {
   const app = express()
-  const options = Object.assign({}, moduleOptions, { expressAppInstance: app })
+  const options = Object.assign({}, moduleOptions, {
+    expressAppInstance: app,
+    isDev: this.options.dev
+  })
 
   app.use(remountRouter(options))
   this.addServerMiddleware({ path: '/', handler: app._router })
