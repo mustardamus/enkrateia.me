2655edea45cce3a9646088fde8a214f4da05e7f3
Sebastian Senf <contact@mustardamus.com>
2017-09-08 12:53:57 +0200

    create pre-commit symlink on postinstall

--- tasks/git/pre-commit.js
+++ tasks/git/pre-commit.js
@@ -3,10 +3,8 @@
 const { join } = require('path')
 const { spawn } = require('child_process')
 
-const eslint = join(__dirname, '../../node_modules/.bin/eslint')
-const options = '--ext .js,.vue --ignore-path .gitignore .'.split(' ')
-const lint = spawn(eslint, options, { stdio: ['inherit', 'inherit', 'inherit'] })
+const bin = join(__dirname, '../../node_modules/.bin/eslint')
+const opt = '--ext .js,.vue --ignore-path .gitignore .'.split(' ')
+const lint = spawn(bin, opt, { stdio: ['inherit', 'inherit', 'inherit'] })
 
-lint.on('close', code => {
-  process.exit(code)
-})
+lint.on('close', code => process.exit(code))
