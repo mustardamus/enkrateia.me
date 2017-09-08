9cf257ea70b5942aca21d0ebe5ae2cd10a23e8ff
Sebastian Senf <contact@mustardamus.com>
2017-09-08 12:37:57 +0200

    huh

--- components/ContactForm.vue
+++ components/ContactForm.vue
@@ -91,7 +91,7 @@ export default {
     },
 
     validateEmail (email) {
-      // xxceslint-disable-next-line
+      // xxcceslint-disable-next-line
       const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
       return re.test(email)
     },
--- tasks/git/pre-commit.js
+++ tasks/git/pre-commit.js
@@ -11,23 +11,3 @@ lint.on('close', code => {
   console.log('c', code)
   process.exit(code)
 })
-
-/*
-var exec = require('child_process').exec
-var fs = require('fs')
-var eslint = require('../../node_modules/eslint/lib/cli')
-
-var files
-
-var child = exec('git diff-index --cached --name-only HEAD', function (error, stdout, stderr) {
-  files = stdout.split('\n').filter(function (i) {
-    return /\.js$/.test(i) && fs.existsSync(i)
-  })
-  if (files.length === 0) {
-    return
-  }
-
-  var exitCode = eslint.execute.apply(this, files)
-  process.exit(exitCode)
-})
-*/
